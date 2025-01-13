5f402247898aa11c49a95f6ecb88f3507a0ef2bb
JuigeS <jusuut@gmail.com>
2025-01-13 14:03:11 +0200

    Delete view_tasks.py

--- view_tasks.py
+++
@@ -1,7 +0,0 @@
-# git ryhmä harjoitus Jani (lead), b Jarmo, c Jussi, d Maiju 
-
-def view_tasks(tasks):
-	print("This is the list of existing tasks")
-	index = 0
-	while index < len(tasks):  # while items in list
-		print(f'{index+1}. {tasks[index]}')    # print item
