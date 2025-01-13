e3d9371553c033e10fce896c1c938c0b9bacd34e
maikarhu2 <maiju.karhu@windowslive.com>
2025-01-13 12:48:17 +0200

    Added import functions line and initializing functions

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ With this program you can create and edit a To Do -list
 
 '''
 
+from to do import add_task, view_tasks, remove_task
+
+# Run the program
+if __name__ == '__main__':
+    # User adds task
+    add_task()
+
+    # User views task
+    view_tasks()
+
+    # User removes task
+    
