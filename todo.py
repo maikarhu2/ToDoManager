30774a054d062cd70c5ce8f36ecf5af7b703898d
TraDukTer <57787752+TraDukTer@users.noreply.github.com>
2025-01-13 12:40:03 +0200

    implement add_task

--- todo.py
+++ todo.py
@@ -1,1 +1,12 @@
-pass
+tasks = []
+
+def add_task() -> bool:
+    new_task = input("Enter task: ")
+    try:
+        tasks.append(new_task)
+    except Exception:
+        print(f"error: {Exception}")
+        return False
+    
+    return True
+    
