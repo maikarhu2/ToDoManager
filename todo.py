6b278ac35b66ced41858aad8e78a4babff6071f2
JarreZ1 <jp033714@edu.taitotalo.fi>
2025-01-13 14:27:25 +0200

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,12 +1,8 @@
-# git ryhmä harjoitus a Jani (lead), b Jarmo, c Jussi, d Maiju 
+# git ryhmä harjoitus Jani (lead), b Jarmo, c Jussi, d Maiju 
 
 def view_tasks(tasks):
 	print("This is the list of existing tasks")
 	index = 0
 	while index < len(tasks):  # while items in list
 		print(f'{index+1}. {tasks[index]}')    # print item
-
-# remove_task
-def remove_task(tasks):
-    answer = int(input('Choose number to remove '))
-    tasks.remove(answer-1)
+		index += index
